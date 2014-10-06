87006baf641e04c9f5e764a03c13c1bcf610cc93
Blake Niemyjski <bniemyjski@gmail.com>
2014-10-06 10:28:36 -0500

    Added a tests console project (to help debugging associations)

---
+++ Source/SchemaHelper.Tests/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using CodeSmith.SchemaHelper;
+using SchemaExplorer;
+using Configuration = CodeSmith.SchemaHelper.Configuration;
+
+namespace SchemaHelper.Tests {
+    internal class Program {
+        private static void Main(string[] args) {
+            string databaseName = "PetShop";
+
+            Configuration.Instance.IncludeFunctions = true;
+            Configuration.Instance.IncludeViews = true;
+
+            Configuration.Instance.CleanExpressions.Add(new Regex("^(sp|tbl|udf|vw|se)_", RegexOptions.IgnoreCase));
+            Configuration.Instance.IgnoreExpressions.Add(new Regex("^dbo.sysdiagrams$", RegexOptions.IgnoreCase));
+            Configuration.Instance.IgnoreExpressions.Add(new Regex("^dbo.aspnet", RegexOptions.IgnoreCase));
+            Configuration.Instance.IgnoreExpressions.Add(new Regex("^dbo.vw_aspnet", RegexOptions.IgnoreCase));
+
+            var database = new DatabaseSchema(new SqlSchemaProvider(), String.Format(@"Data Source=.;Initial Catalog={0};Integrated Security=True", databaseName)) {
+                DeepLoad = true
+            };
+            var provider = new SchemaExplorerEntityProvider(database);
+            var manager = new EntityManager(provider);
+
+            foreach (var entity in manager.Entities) {
+                if (entity.Associations.Count == 0)
+                    continue;
+
+                Console.WriteLine("{0}-{1}", entity.TypeAccess, entity.Name);
+                foreach (var association in entity.Associations) {
+                    Console.WriteLine(String.Format("   {0} IsParent: {1}, AssociationType: {2}, Properties: {3}, Entity Properties: {4}, Foreign Entity Properties: {5}, AssociationKeyName: {6}", association.AssociationKeyName, association.IsParentEntity, association.AssociationType, association.Properties.Count, association.Entity.Properties.Count, association.ForeignEntity.Properties.Count, association.AssociationKeyName));
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Source/SchemaHelper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchemaHelper.Tests")]
+[assembly: AssemblyProduct("SchemaHelper.Tests")]
+[assembly: AssemblyCopyright("Copyright CodeSmith Tools, LLC. 2014")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2e476f51-d267-4bea-bec6-f7121538380a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
